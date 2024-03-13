33d5ca814ba0c37140250adcbff3b2a8fe3d8ba4
Andrey Nazarenko <limelaudio@gmail.com>
2024-03-13 18:52:19 +0200

    update polygons

--- polygons-data/index.ts
+++ polygons-data/index.ts
@@ -1,28 +1,28 @@
- import afganistan from './afg.json';
- import albania from './alb.json';
- import algeria from './dza.json';
- import andorra from './and.json';
- import angola from './ago.json';
- import antiguaAndBarbuda from './atg.json';
+//  import afganistan from './afg.json';
+//  import albania from './alb.json';
+//  import algeria from './dza.json';
+//  import andorra from './and.json';
+//  import angola from './ago.json';
+//  import antiguaAndBarbuda from './atg.json';
  import argentina from './arg.json';
- import armenia from './arm.json';
-//  import australia from './auss.json';
- import austria from './aut.json';
+//  import armenia from './arm.json';
+ import australia from './auss.json';
+//  import austria from './aut.json';
  import anatarctica from './ata.json';
-//  import azerbaijan from './aze.json';
+ import azerbaijan from './aze.json';
 
  const data = [
-  afganistan,
+  // afganistan,
   anatarctica,
-  albania,
-  algeria,
-  andorra,
-  angola,
-  antiguaAndBarbuda,
+  // albania,
+  // algeria,
+  // andorra,
+  // angola,
+  // antiguaAndBarbuda,
   argentina,
-  armenia,
-  // australia,
-  austria,
-  // azerbaijan,
+  // armenia,
+  australia,
+  // austria,
+  azerbaijan,
 ]
 export default data;
