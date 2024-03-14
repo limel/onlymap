a2d7e3bc13c082558762fbf4527fe7643ea016ce
Andrey Nazarenko <limelaudio@gmail.com>
2024-03-14 10:16:23 +0200

    change polygons

--- polygons-data/index.ts
+++ polygons-data/index.ts
@@ -5,8 +5,23 @@
 //  import angola from './ago.json';
 //  import antiguaAndBarbuda from './atg.json';
 //  import argentina from './arg.json';
-//  import armenia from './arm.json';
- import australia from './aus.json';
+ import armenia from './arm.json';
+ import aze1 from './aze/1.json';
+ import aze2 from './aze/2.json';
+ import aze3 from './aze/3.json';
+ import aze4 from './aze/4.json';
+ import aze5 from './aze/5.json';
+ import aze6 from './aze/6.json';
+ import aze7 from './aze/7.json';
+ import aze8 from './aze/8.json';
+ import aze9 from './aze/9.json';
+ import aze10 from './aze/10.json';
+ import aze11 from './aze/11.json';
+ import aze12 from './aze/12.json';
+ import aze13 from './aze/13.json';
+ import aze14 from './aze/14.json';
+
+ import australia from './auss.json';
 //  import austria from './aut.json';
 //  import anatarctica from './ata.json';
 //  import azerbaijan from './aze.json';
@@ -20,9 +35,23 @@
   // angola,
   // antiguaAndBarbuda,
   // argentina,
-  // armenia,
+  armenia,
   australia,
   // austria,
   // azerbaijan,
+  aze1,
+  aze2,
+  aze3,
+  aze4,
+  aze5,
+  aze6,
+  aze7,
+  aze8,
+  aze9,
+  aze10,
+  aze11,
+  aze12,
+  aze13,
+  aze14,
 ]
 export default data;
