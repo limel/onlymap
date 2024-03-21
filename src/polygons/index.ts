8e4ec6db48e292c9c0156c1e2b8399730aabcaf2
Andrey Nazarenko <limelaudio@gmail.com>
2024-03-21 17:13:47 +0200

    update polygons

--- src/polygons/index.ts
+++ src/polygons/index.ts
@@ -2,56 +2,40 @@ import arm from './arm.json'
 import cambodia from './cambodia.json'
 import cameroon from './cameroon.json'
 import capeVerde from './cape-verde.json'
-import caf from './central-african-republic.json'
-import chad from './chad.json'
-import chilie from './chilie.json'
-import china from './china.json'
-import colombia from './colombia.json'
-import comoros from './comors.json'
-import costaRica from './costa-rica.json'
-import cuba from './cuba.json'
-import cyprus from './cyprus.json'
-import croatia from './croatia.json'
-import czech from './czech.json'
-import congo from './congo.json'
-import denmark from './denmark.json'
+// import caf from './central-african-republic.json'
+import caf from './caf.json'
 import djibouti from './djibouti.json'
 import dominica from './dominica.json'
 import dominicanRepublic from './dominican-republic.json'
 import esatTrimor from './east-timor-leste.json'
 import ecuador from './ecuador.json'
 import egypt from './egypt.json'
-// import elSalvador from './el-salvador.json'
-// import equatorialGuinea from './equatorial-guinea.json'
-// import eritrea from './eritrea.json'
-// import estonia from './estonia.json'
-// import eswatini from './eswatini.json'
+import elSalvador from './el-salvador.json'
+import equatorialGuinea from './equatorial-guinea.json'
+import eritrea from './eritrea.json'
+import estonia from './estonia.json'
+import eswatin from './eswatin.json'
+import ethiopia from './ethiopia.json'
+import fiji from './fiji.json'
+import finland from './finland.json'
 
 // need refactoring for type geoJsonData
 const data: any[] = [
-  arm,
-  cambodia,
-  cameroon,
-  capeVerde,
   caf,
-  chad,
-  chilie,
-  croatia,
-  china,
-  colombia,
-  comoros,
-  costaRica,
-  cuba,
-  cyprus,
-  czech,
-  congo,
-  // denmark,
-  // djibouti,
-  // dominica,
-  // dominicanRepublic,
-  // esatTrimor,
-  // ecuador,
-  // egypt,
+  dominica,
+  dominicanRepublic,
+  djibouti,
+  esatTrimor,
+  ecuador,
+  egypt,
+  elSalvador,
+  equatorialGuinea,
+  eritrea,
+  estonia,
+  eswatin,
+  ethiopia,
+  fiji,
+  finland,
 ]
 
 export default data
