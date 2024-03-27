b3ca1295ba3607c2bb5a6468db637bc2e70872b4
Andrey Nazarenko <limelaudio@gmail.com>
2024-03-27 17:34:56 +0200

    add polygons

--- src/polygons/index.ts
+++ src/polygons/index.ts
@@ -1,44 +1,40 @@
 import armenia from './armenia.json'
 import aze from './aze.json'
+//
 
-import serbia from './serbia.json'
-import bosnia from './bosnia.json'
-import caf from './caf.json'
 import djibouti from './djibouti.json'
 import dominica from './dominica.json'
 import dominicanRepublic from './dominican-republic.json'
-import esatTrimor from './east-timor-leste.json'
+import timorLeste from './timor-leste.json'
 import ecuador from './ecuador.json'
-import egypt from './egypt.json'
-import elSalvador from './el-salvador.json'
-import equatorialGuinea from './equatorial-guinea.json'
-import eritrea from './eritrea.json'
-import estonia from './estonia.json'
-import eswatin from './eswatin.json'
 import ethiopia from './ethiopia.json'
-import fiji from './fiji.json'
-import finland from './finland.json'
+import ghana from './ghana.json'
+import grenada from './grenada.json'
+import guatemala from './guatemala.json'
+import guyana from './guyana.json'
+import haiti from './haiti.json'
+import hungary from './hungary.json'
+import iceland from './iceland.json'
+import india from './india.json'
 
 // need refactoring for type geoJsonData
 const data: any[] = [
   armenia,
-  aze,
-  dominica,
-  serbia,
-  bosnia,
-  dominicanRepublic,
+  // aze,
   djibouti,
-  esatTrimor,
+  dominica,
+  dominicanRepublic,
+  timorLeste,
   ecuador,
-  egypt,
-  elSalvador,
-  equatorialGuinea,
-  eritrea,
-  estonia,
-  eswatin,
   ethiopia,
-  fiji,
-  finland,
+  ghana,
+  grenada,
+  guatemala,
+  guyana,
+  haiti,
+  hungary,
+  iceland,
+  india,
 ]
 
 export default data
