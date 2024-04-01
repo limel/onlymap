ba806062ee927e4d2bc9e8245d3391a584f76f82
Andrey Nazarenko <limelaudio@gmail.com>
2024-04-01 23:01:23 +0300

    add polygons

--- src/polygons/index.ts
+++ src/polygons/index.ts
@@ -15,6 +15,7 @@ import germany from './germany.json'
 import ghana from './ghana.json'
 import grenada from './grenada.json'
 import guatemala from './guatemala.json'
+import guinea from './guinea.json'
 import guyana from './guyana.json'
 import haiti from './haiti.json'
 import hungary from './hungary.json'
@@ -23,18 +24,19 @@ import iceland from './iceland.json'
 
 // need refactoring for type geoJsonData
 const data: any[] = [
-  armenia,
+  // armenia,
   gabon,
   gambia,
   georgia,
-  germany,
-  ghana,
-  grenada,
+  guinea,
+  // germany,
+  // ghana,
+  // grenada,
   guatemala,
-  guyana,
-  haiti,
-  hungary,
-  iceland,
+  // guyana,
+  // haiti,
+  // hungary,
+  // iceland,
   // india,
 ]
 
