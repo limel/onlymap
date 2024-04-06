85aeec44eccff711e3c0ce5b3d7a762e5b4d7ca3
Andrey Nazarenko <limelaudio@gmail.com>
2024-04-06 09:04:24 +0300

    add polygons

--- src/polygons/index.ts
+++ src/polygons/index.ts
@@ -1,11 +1,16 @@
-import guatemala from './guatemala.json'
-import guinea from './guinea.json'
-import iceland from './iceland.json'
-import guineaBissau from './guinea-bissau.json'
-import iran from './iran.json'
-import iraq from './iraq.json'
-import israel from './israel.json'
+// import guatemala from './guatemala.json'
+// import guinea from './guinea.json'
+// import iceland from './iceland.json'
+// import guineaBissau from './guinea-bissau.json'
+// import iran from './iran.json'
+// import iraq from './iraq.json'
+// import israel from './israel.json'
+import honduras from './honduras.json'
+import italy from './italy.json'
+import ivoryCost from './ivory-cost.json'
+import jamaica from './jamaica.json'
+import jordan from './jordan.json'
 
-const data: any[] = [guatemala, guinea, guineaBissau, iceland, iran, iraq, israel]
+const data: any[] = [honduras, italy, ivoryCost, jamaica, jordan]
 
 export default data
