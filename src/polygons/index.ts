6cc83485393346dcb68d24eb22cf7328f4171007
Andrey Nazarenko <limelaudio@gmail.com>
2024-04-10 13:15:09 +0300

    add polygons

--- src/polygons/index.ts
+++ src/polygons/index.ts
@@ -5,12 +5,18 @@
 // import iran from './iran.json'
 // import iraq from './iraq.json'
 // import israel from './israel.json'
-import honduras from './honduras.json'
-import italy from './italy.json'
-import ivoryCost from './ivory-cost.json'
-import jamaica from './jamaica.json'
-import jordan from './jordan.json'
+// import honduras from './honduras.json'
+// import italy from './italy.json'
+// import ivoryCost from './ivory-cost.json'
+// import jamaica from './jamaica.json'
+// import jordan from './jordan.json'
+// import aze from './aze.json'
+// import armenia from './armenia.json'
+import kuwait from './kuwait.json'
+import kazakhstan from './kazakhstan.json'
+import kyrgyzstan from './kyrgyzstan.json'
+import kosovo from './kosovo.json'
 
-const data: any[] = [honduras, italy, ivoryCost, jamaica, jordan]
+const data: any[] = [kuwait, kazakhstan, kyrgyzstan, kosovo]
 
 export default data
